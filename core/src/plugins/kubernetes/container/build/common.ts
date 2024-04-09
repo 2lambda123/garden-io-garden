922ed7647b0f3c9b2a0a8ea5af2eb6bb1dd98a29
Vladimir Vagaytsev <vladimir.vagaitsev@gmail.com>
2024-04-09 15:52:06 +0200

    chore: remove unused constant

--- core/src/plugins/kubernetes/container/build/common.ts
+++ core/src/plugins/kubernetes/container/build/common.ts
@@ -35,7 +35,6 @@ import type { StringMap } from "../../../../config/common.js"
 import { LogLevel } from "../../../../logger/logger.js"
 
 export const inClusterBuilderServiceAccount = "garden-in-cluster-builder"
-export const sharedBuildSyncDeploymentName = "garden-build-sync"
 export const utilContainerName = "util"
 export const utilRsyncPort = 8730
 export const utilDeploymentName = "garden-util"
